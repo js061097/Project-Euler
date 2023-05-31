c885ae78e1822c54222fe859b5bf489c7ac2aded
Jason Stanley <js41074546@gmail.com>
2023-05-31 01:08:21 -0400

    Optimized

--- Problem Archives/p0002.py
+++ Problem Archives/p0002.py
@@ -1,23 +1,18 @@
-
-
-
-def fibsum(limit):
+def compute(f,s,lim):
     """
     returns the sum of even elements of the fibonacci series starting with 1 and 2
     arguments:
+      f - first number of the fibonacci series
+      s - second number of the fibonacci series
       limit - maximum value of the fibonacci series term till which the sum is evaluated
     """
-    prev = 1
-    curr = 2
-    x = prev + curr
-    sum = 2
-    while(x<limit):
-        x = prev + curr
-        sum = sum + (x if x%2==0 else 0)
-        prev = curr
-        curr = x
+    sum = 0
+    while (s<lim):
+        if(not s%2):
+            sum+=s
+        s = f + s
+        f = s - f
     return sum
-    
 
 if __name__ == "__main__":
-    print(fibsum(4000000))
+    print(compute(1,2,4000000))
